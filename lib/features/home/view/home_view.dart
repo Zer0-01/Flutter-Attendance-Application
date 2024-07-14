99d3ef9939b84926c8e3b1bdf0d240ae1a49e0c9
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-14 13:15:59 +0800

    feat: add home feature
    This commit is about add the home feature
    Create directory for the home feature and sub directory for the view and the view model

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -11,10 +11,69 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [Text('Home Page')],
+        appBar: AppBar(
+          actions: const <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        drawer: const Drawer(),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 24.0, top: 16.0, right: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Hi, User',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisSpacing: 8.0,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  children: const [
+                    Card(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.qr_code,
+                            size: 64.0,
+                          ),
+                          Text(
+                            'Scanner',
+                            style: TextStyle(fontSize: 32.0),
+                          )
+                        ],
+                      ),
+                    ),
+                    Card(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            'TBD',
+                            style: TextStyle(fontSize: 32.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/splash/view_model/splash_view_model.dart
+++ lib/features/splash/view_model/splash_view_model.dart
@@ -4,7 +4,7 @@ import 'package:flutter_attendance_application/features/home/view/home_view.dart
 class SplashViewModel extends ChangeNotifier {
   void goToHomeView(BuildContext context) {
     Future.delayed(const Duration(milliseconds: 5000), () {
-      Navigator.pushNamed(context, HomeView.id);
+      Navigator.pushReplacementNamed(context, HomeView.id);
     });
   }
 }
