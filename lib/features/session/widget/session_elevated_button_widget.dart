24136a23a1090d663647ee4d923a9f11e588071b
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-26 22:59:03 +0800

    feat: create session feature

---
+++ lib/features/session/widget/session_elevated_button_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SessionElevatedButtonWidget extends StatelessWidget {
+  final void Function()? onPressed;
+  final String label;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final Color borderColor;
+  const SessionElevatedButtonWidget(
+      {super.key,
+      this.onPressed,
+      required this.label,
+      this.backgroundColor,
+      this.foregroundColor,
+      required this.borderColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        foregroundColor: foregroundColor,
+        side: BorderSide(color: borderColor),
+        textStyle: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      child: Text(label),
+    );
+  }
+}
---
+++ lib/res/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff3F2305);
+  static const Color secondaryColor = Color(0xffF5F5F5);
+
+  
+}
