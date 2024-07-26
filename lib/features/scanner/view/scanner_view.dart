336502bbf001aca4bf8719eb4ad663a3ab019e0d
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-26 23:37:18 +0800

    feat: create folder for scanner feature

--- lib/features/scanner/view/scanner_view.dart
+++ lib/features/scanner/view/scanner_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_attendance_application/features/scanner/view_model/scanner_view_model.dart';
 
 class ScannerView extends StatefulWidget {
   static const String id = 'scanner_view';
@@ -10,6 +11,13 @@ class ScannerView extends StatefulWidget {
 }
 
 class _ScannerViewState extends State<ScannerView> {
+  ScannerViewModel vm = ScannerViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
