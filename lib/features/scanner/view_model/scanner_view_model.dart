5358dece306a0bf0d9a83a36aef920d612f75d82
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-07-28 11:18:43 +0800

    feat: add dismiss method, import audio player

--- lib/features/scanner/view_model/scanner_view_model.dart
+++ lib/features/scanner/view_model/scanner_view_model.dart
@@ -1,11 +1,22 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class ScannerViewModel extends ChangeNotifier {
   Barcode? barcode;
+  final player = AudioPlayer();
 
   void handleBarcode(BarcodeCapture barcodeCapture) {
     barcode = barcodeCapture.barcodes.firstOrNull;
     notifyListeners();
+  }
+
+  void cancel(BuildContext context) {
+    _dismiss(context);
+  }
+
+  //private method
+  void _dismiss(BuildContext context) {
+    Navigator.pop(context);
   }
 }
